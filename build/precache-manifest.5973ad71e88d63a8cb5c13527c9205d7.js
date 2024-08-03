1e43655c7becb3a32114239d24714d787fee8efa
Isac Gomes <isacfisio@gmail.com>
2024-08-02 22:11:59 -0300

    mudanças

---
+++ build/precache-manifest.5973ad71e88d63a8cb5c13527c9205d7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bd38e46959f40512f38d4bb86f3fd50",
+    "url": "/smartTime/index.html"
+  },
+  {
+    "revision": "1c2fcb27f2541f254846",
+    "url": "/smartTime/static/css/2.a94b31b9.chunk.css"
+  },
+  {
+    "revision": "dccea58cf6cf97ba83da",
+    "url": "/smartTime/static/css/main.fb50cb86.chunk.css"
+  },
+  {
+    "revision": "1c2fcb27f2541f254846",
+    "url": "/smartTime/static/js/2.f7c5dda2.chunk.js"
+  },
+  {
+    "revision": "c87ae3f2a437a1b2df8d",
+    "url": "/smartTime/static/js/3.177faaa1.chunk.js"
+  },
+  {
+    "revision": "dccea58cf6cf97ba83da",
+    "url": "/smartTime/static/js/main.f22b6bc4.chunk.js"
+  },
+  {
+    "revision": "e30a8b55e97253a25826",
+    "url": "/smartTime/static/js/runtime~main.08ea9b2b.js"
+  },
+  {
+    "revision": "6225992b1ae7a8e0287c0811d92e876c",
+    "url": "/smartTime/static/media/logo_transparent.6225992b.png"
+  }
+]);
